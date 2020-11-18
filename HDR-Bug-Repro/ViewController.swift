58f288d7d419fecd8d505cbdd3b84b1c7162a341
Josh Pyles <josh@cocoon.com>
2020-11-18 12:00:16 -0800

    Initial Commit

---
+++ HDR-Bug-Repro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HDR-Bug-Repro
+//
+//  Created by Josh Pyles on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
